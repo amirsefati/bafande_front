f26f676be8f5d00066420b62ddf5d5b3ca93c33b
bafande <bafandeh.mohammad@gmail.com>
2020-12-25 16:44:44 +0330

    added footer to components

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="footer">
+    <a href="#">Facebook</a>
+    <a href="#">Twitter</a>
+    <a href="#">Instagram</a>
+    <p>© Copyright 2020 iBooks</p>
+  </div>
+</template>
+
+<style scoped>
+#footer {
+  background-color: azure;
+  padding: 7% 15%;
+}
+a {
+  font-family: serif;
+  text-decoration: none;
+  color: rgb(7, 52, 121);
+  margin: 20px;
+}
+p {
+  font-family: serif;
+  color: aqua;
+}
+</style>
+
+<script>
+export default {};
+</script>
